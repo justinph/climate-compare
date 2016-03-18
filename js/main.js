0de242682ccf764fcc8607ec31dabbfe4a3fce97
Justin Heideman <justin.heideman@nytimes.com>
2016-03-18 15:24:45 -0400

    ongoing work

--- js/main.js
+++ js/main.js
@@ -21,14 +21,16 @@ CC.updateGraph = function (event) {
     if (!val){ return; }
     if (!CC.doneSetup){ CC.setupGraph(); };
 
-    CC.clearPrevGraph(idx);
+    //CC.clearPrevGraph(idx);
     CC.loadData(val, idx);
 
 };
 
-CC.clearPrevGraph = function (idx){
-
-};
+// CC.clearPrevGraph = function (idx){
+//     // this.svg.select(".line")   // change the line
+//     //         .duration(750)
+//     //         .attr("d", valueline(data));
+// };
 
 CC.setupGraph = function (){
     var margin = {top: 20, right: 20, bottom: 30, left: 50};
@@ -105,20 +107,48 @@ CC.loadData = function (stationId, idx){
             return d;
         })
         //do stuff with it
-        .get(function (error, rows) {
-
-            CC.svg.append('path')
-                .datum(rows)
-                .attr('class', 'temp-range min-max-'+idx)
-                .attr('data-idx', idx)
-                .attr('d', CC.tminTmaxArea);
-
-            CC.svg.append('path')
-                .datum(rows)
-                .attr('class', 'avg-temp avg-'+idx)
-                .attr('data-idx', idx)
-                .attr('d', CC.tavgLine);
+        .get(function (error, data) {
+            CC.drawLine(idx, data);
         });
 };
+
+CC.drawLine = function (idx, data){
+    var svg = d3.select('body').transition();
+
+    var minMaxSel = svg.select('temp-range.min-max-'+idx);
+    var avgSel = svg.select('.avg-temp.avg-'+idx);
+
+    if (minMaxSel.empty() && avgSel.empty()) {
+        //haven't been drawn yet
+        CC.svg.append('path')
+            .datum(data)
+            .attr('class', 'temp-range min-max-'+idx)
+            .attr('data-idx', idx)
+            .attr('d', CC.tminTmaxArea);
+
+        CC.svg.append('path')
+            .datum(data)
+            .attr('class', 'avg-temp avg-'+idx)
+            .attr('data-idx', idx)
+            .attr('d', CC.tavgLine);
+    } else {
+        //update existing
+        minMaxSel
+            .duration(750)
+            .datum(data)
+            .attr('d', CC.tminTmaxArea);
+
+        avgSel
+            .duration(750)
+            .datum(data)
+            .attr('d', CC.tavgLine);
+
+    }
+
+
+
+};
+
+//CC.updateRows()
 
 CC.init();
